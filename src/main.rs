3260fa3abfc3896abdac2343123eaab3bed8321c
tanndlin <tanndlin@gmail.com>
2025-12-02 17:39:53 -0500

    Reformat output

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 mod solutions;
 
-use crate::solutions::{aoc2::AoC2, solution::Solution};
+use crate::solutions::{aoc1::AoC1, aoc2::AoC2, solution::Solution};
 
 fn main() {
-    let sol = AoC2::new();
-    let day = 2;
-    println!("Advent of Code Day {}", day);
-    println!(
-        "Sample\n\tPart 1: {}\n\tPart 2: {}",
-        sol.solve1(format!("{}a", day).as_str()),
-        sol.solve2(format!("{}a", day).as_str())
-    );
-    println!(
-        "Real\n\tPart 1: {}\n\tPart 2: {}",
-        sol.solve1(format!("{}b", day).as_str()),
-        sol.solve2(format!("{}b", day).as_str())
-    );
+    AoC1::new().run(1);
+    AoC2::new().run(2);
 }
--- src/solutions/solution.rs
+++ src/solutions/solution.rs
@@ -3,6 +3,20 @@ use std::path::Path;
 pub trait Solution {
     fn new() -> Self;
 
+    fn run(&self, day: u8) {
+        println!("Advent of Code Day {}", day);
+        println!(
+            "Part 1 Sample: {:20} | Real:   {:20}",
+            self.solve1(format!("{}a", day).as_str()),
+            self.solve1(format!("{}b", day).as_str())
+        );
+        println!(
+            "Part 2 Sample: {:20} | Real:   {:20}\n",
+            self.solve2(format!("{}a", day).as_str()),
+            self.solve2(format!("{}b", day).as_str())
+        );
+    }
+
     fn solve1(&self, input: &str) -> u64 {
         let filename = format!("src/inputs/{}.txt", input);
         let path = Path::new(&filename);
